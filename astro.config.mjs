417bf55ab9ac118e557c6459fa2109f46636ec6e
Csanchez <casanchez@nrmweb.com.mx>
2025-07-25 14:56:15 -0600

    corrigiendo errires

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stereociendigital.mx',
+  site: 'https://sabrositadigital.mx',
   integrations: [mdx(), sitemap({ sitemap: '/sitemap.xml' }), tailwind()],
   devToolbar: {
     enabled: true
