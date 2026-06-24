25204e7350eb0ddd0731962b3ec43e93f092fe1c
Csanchez <casanchez@nrmweb.com.mx>
2026-06-24 13:25:59 -0600

    quitando 1410

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sabrosita 590 - 1410 AM';
-export const SITE_DESCRIPTION = 'Sabrosita 590-1410 AM somos salsa, cumbia y todos los géneros tropicales que nos convierte en la estación de radio preferida de los que les gusta bailar y gozar de la vida.';
+export const SITE_TITLE = 'Sabrosita 590 AM';
+export const SITE_DESCRIPTION = 'Sabrosita 590 AM somos salsa, cumbia y todos los géneros tropicales que nos convierte en la estación de radio preferida de los que les gusta bailar y gozar de la vida.';
